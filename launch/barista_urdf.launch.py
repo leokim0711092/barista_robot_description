90e5d44a898737192d6e49b1eb6da8d3c1b40058
leokim0711092 <leokim10073@gmail.com>
2023-10-13 04:17:37 +0000

    Launch gazebo and joint state publisher

--- launch/barista_urdf.launch.py
+++ launch/barista_urdf.launch.py
@@ -1,9 +1,13 @@
 import os
-
+import random
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.substitutions import Command
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_prefix
 
 # this is the function launch  system will look for
 def generate_launch_description():
@@ -15,6 +19,14 @@ def generate_launch_description():
     ####### DATA INPUT END ##########
     print("Fetching URDF ==>")
     robot_desc_path = os.path.join(get_package_share_directory(package_description), "urdf", urdf_file)
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+        
+    # Gazebo launch
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py'),
+        )
+    )    
 
     # Robot State Publisher
 
@@ -27,6 +39,15 @@ def generate_launch_description():
         output="screen"
     )
 
+    # Joint State Publisher
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        output='screen'
+    )
+
+    #rviz
     rviz_config_dir = os.path.join(get_package_share_directory(package_description), 'rviz', 'urdf.rviz')
 
 
@@ -38,11 +59,42 @@ def generate_launch_description():
             parameters=[{'use_sim_time': True}],
             arguments=['-d', rviz_config_dir])
 
+    #spawn robot in Gazebo
+
+    # Position and orientation
+    # [X, Y, Z]
+    position = [0.0, 0.0, 0.2]
+    # [Roll, Pitch, Yaw]
+    orientation = [0.0, 0.0, 0.0]
+    # Base Name or robot
+    robot_base_name = "barista_robot"
+
+
+    entity_name = robot_base_name+"-"+str(int(random.random()*100000))
+
+    # Spawn ROBOT Set Gazebo
+    spawn_robot = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        name='spawn_entity',
+        output='screen',
+        arguments=['-entity',
+                   entity_name,
+                   '-x', str(position[0]), '-y', str(position[1]
+                                                     ), '-z', str(position[2]),
+                   '-R', str(orientation[0]), '-P', str(orientation[1]
+                                                        ), '-Y', str(orientation[2]),
+                   '-topic', '/robot_description'
+                   ]
+    )
 
     # create and return launch description object
     return LaunchDescription(
         [            
             robot_state_publisher_node,
-            rviz_node
+            rviz_node,
+            gazebo,
+            spawn_robot,
+            joint_state_publisher_node
         ]
     )
